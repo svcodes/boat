53ff8377f703942d331362eac8563a04bbdad72a
svcodes <suhasvoolla@gmail.com>
2020-11-18 21:09:10 -0600

    add version

--- cogs/info.py
+++ cogs/info.py
@@ -61,7 +61,7 @@ class Information(commands.Cog):
             name=f"Developer{'' if len(self.config.owners) == 1 else 's'}",
             value=', '.join([str(self.bot.get_user(x)) for x in self.config.owners]),
             inline=True)
-        embed.add_field(name="Library", value="discord.py", inline=True)
+        embed.add_field(name="Library", value=f"discord.py v{discord.__version__}", inline=True)
         embed.add_field(name="Servers", value=f"{len(ctx.bot.guilds)} ( avg: {avgmembers} users/server )", inline=True)
         embed.add_field(name="Commands loaded", value=len([x.name for x in self.bot.commands]), inline=True)
         embed.add_field(name="RAM", value=f"{ramUsage:.2f} MB", inline=True)
