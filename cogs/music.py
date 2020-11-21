d0da42f6347a413a9c2ba3cf1257f28c4c0b8849
svcodes <suhasvoolla@gmail.com>
2020-11-20 22:58:20 -0600

    pp

--- cogs/music.py
+++ cogs/music.py
@@ -20,7 +20,7 @@ class Music(commands.Cog):
         self.bot = bot
 
         if not hasattr(bot, 'lavalink'):  # This ensures the client isn't overwritten during cog reloads.
-            bot.lavalink = lavalink.Client(bot.user.id)
+            bot.lavalink = lavalink.Client(self.bot.user.id)
             bot.lavalink.add_node('127.0.0.1', 2333, 'youshallnotpass', 'eu', 'default-node')  # Host, Port, Password, Region, Name
             bot.add_listener(bot.lavalink.voice_update_handler, 'on_socket_response')
 
