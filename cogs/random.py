3884f9f2bee40b349dd4228911050df2e167aca1
svcodes <suhasvoolla@gmail.com>
2021-01-21 18:14:26 +0000

    add runpy

--- cogs/random.py
+++ cogs/random.py
@@ -1,11 +1,17 @@
 import discord
 from discord.ext import commands
+from utils import http
 
 
 class Random(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
     
+    @commands.command()
+    async def runpy(self, ctx, *, code)
+        json = {"language": "python3", "source": code}
+        returned = await http.post(url="https://emkc.org/api/v1/piston/execute", json=json)
+        await ctx.send(returned)
 
     
 def setup(bot):
