4f6612a9e48d632a06fd8675a9af7ac6273b59d0
zero <suhasvoolla@gmail.com>
2021-03-23 15:42:58 -0500

    trash osu command

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,22 +1,35 @@
 import discord
 from discord.ext import commands
-from utils import default, http, permissions
+from utils import default, http
 import urllib.parse
 from math import floor
+
 
 class Osu(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
         self.config = default.get("config.json")
         self.osu_key = self.config.osu_api_key
-    
+
     @commands.group()
     async def osu(self, ctx):
         if ctx.invoked_subcommand is None:
-            await ctx.send('Please include a subcommand! (.osu user <username>)')
-    
+            await ctx.send('Please include a subcommand! (.osu user <username> | .osu rs <username>)')
+
+    @osu.command(aliases=['rs'])
+    async def recent(self, ctx, username, result: int = 1):
+        """recent map stats"""
+        api = await http.get(url=f"https://osu.ppy.sh/api/get_user_recent?k={self.osu_key}&u={urllib.parse.quote_plus(username)}",res_method = "json")
+        if len(api) == 0:
+            return await ctx.send("error no data found")
+        data = api[(result-1)]
+        await ctx.send(f"""data
+mapid: {data['beatmap_id']}
+score: {data['score']}""")
+
     @osu.command()
     async def user(self, ctx, username):
+        """user statistics for osu"""
         api = await http.get(url=f"https://osu.ppy.sh/api/get_user?k={self.osu_key}&u={urllib.parse.quote_plus(username)}", res_method="json")
         data = api[0]
         embed = discord.Embed(title = f"osu! stats for {data['username']}", colour = 0xff66aa)
@@ -41,8 +54,6 @@ class Osu(commands.Cog):
         embed.set_image(url=f"http://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname={urllib.parse.quote_plus(username)}&pp=2&onlineindicator=undefined&xpbar&xpbarhex")
 
         await ctx.send(embed=embed)
-    
-    
 
     
 def setup(bot):
