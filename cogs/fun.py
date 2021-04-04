4f86efea38c143ac399544ee1675ac14bde69258
zero <suhasvoolla@gmail.com>
2021-04-03 22:16:55 -0500

    fix: embed=embed moment

--- cogs/fun.py
+++ cogs/fun.py
@@ -59,7 +59,7 @@ class Fun_Commands(commands.Cog):
         for num, option in enumerate(answers):
             desc += f"{EMOJI[num]} {option}"
         embed = discord.Embed(title=question, colour= discord.Colour(0x7289da), description=desc)
-        await ctx.send(embed)
+        await ctx.send(embed=embed)
 
 
 
