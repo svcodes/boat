280948af2e9dc2dc9a730590c7648987465c5d0b
zero <suhasvoolla@gmail.com>
2021-04-03 21:42:00 -0500

    feat: add a clientsession for the bot

--- utils/data.py
+++ utils/data.py
@@ -2,6 +2,7 @@ import discord
 
 from utils import permissions, default
 from discord.ext.commands import AutoShardedBot, DefaultHelpCommand
+import aiohttp
 
 config = default.get("config.json")
 
@@ -10,6 +11,7 @@ class Bot(AutoShardedBot):
     def __init__(self, *args, prefix=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.prefix = prefix
+        self.session = aiohttp.ClientSession()
 
     async def on_message(self, msg):
         if not self.is_ready() or msg.author.bot or not permissions.can_send(msg):
